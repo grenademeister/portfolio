f44f1857884ae63a3bb0a431d3d172ad4891c93e
Hyeokgi Kim <khkryan0905@gmail.com>
2025-12-17 23:05:38 +0900

    feat: add static entry page

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,14 @@ import path from 'path';
 export default defineConfig({
   base: 'portfolio',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        app: path.resolve(__dirname, 'app.html'),
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
